ce0a64dcedc1c9785b41d0fd99bfa819c8268921
Edgar guerra <reinaldo316@gmail.com>
2023-04-20 08:49:49 -0500

    comentarios en el codigo

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
 
 
+#ordena una lista en orden ascendentey la devuelve ordenada
 def sort_list(items, ascending=True):
     if not isinstance(items, list):
         raise RuntimeError(f"No puede ordenar {type(items)}")
@@ -17,10 +18,16 @@ def sort_list(items, ascending=True):
     return sorted(items, reverse=(not ascending))
 
 
+#elimina duplicados de un ista de elementos y devuelve la lista resultante
 def remove_duplicates_from_list(items):
     return list(set(items))
 
 
+"""
+este bloque e codigo verific sise han proporcionado dos argumentos al script, 
+si es asi los utiliza como el nombre del archivo y un indicador booleano para eliminar duplicados
+si no se daningun argumento se imprime un error y sale del script
+"""
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
     remove_duplicates = DEFAULT_DUPLICATES
@@ -33,6 +40,10 @@ if __name__ == "__main__":
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
+    """
+    en este bloque de codigo se lee las palabrasdel archivo especificado, encaso de no tener un archivo
+    se crea una lista de palabras por defecto, de ahi s elimina losduplicados si es necesario
+    """   
     file_path = os.path.join(".", filename)
     if os.path.isfile(file_path):
         word_list = []
@@ -45,5 +56,5 @@ if __name__ == "__main__":
 
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
-
+#se imprime la lista ordenada de palabras
     print(sort_list(word_list))
